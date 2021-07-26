c21ff3ccf2e9f5ac04022ec4cf32622dc263f542
Robert Forget <robforget@gmail.com>
2021-07-25 22:17:13 -0400

    progress

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -3,7 +3,6 @@
 
 import random
 
-
 #Prompting user on how to play
 possible_actions = ["rock", "paper", "scissors"]
 computer_action = random.choice(possible_actions)
@@ -17,10 +16,24 @@ while True:
   print("Please select a 'draw' or input q to quit:")
 #Storing user's choice of rps
   mychoice = input()
-
-#input check/translator
-#checkinput() function should start here.
-if mychoice != "r" or "p" or "s" or "q":
+  if mychoice != "r" or "p" or "s" or "q":
     print("Invalid input.")
     print("Please input either 'r','p' or 's' input 'q' to quit.")
-    mychoice = input()
+    continue
+  elif mychoice == "r":
+      mychoice = "rock"
+      break
+
+  elif mychoice == "p":
+      mychoice = "paper"    
+      break
+
+  elif mychoice == "s":
+      mychoice = "scissors"
+      break
+
+  elif mychoice == "q":
+      print("Quitting...")
+      quit()    
+
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
