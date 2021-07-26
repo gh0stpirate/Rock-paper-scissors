7d299972271da26a5dfa335e82084a68eed2f531
Robert Forget <robforget@gmail.com>
2021-07-25 20:23:35 -0400

    init and laying out skeleton

---
+++ rockpaperscissors.py
@@ -0,0 +1,20 @@
+#Rock paper scissors test
+
+#important code layout
+#import random
+#mylist = ["apple", "banana", "cherry"]
+#print(random.choice(mylist))
+
+import random
+
+#Prompting user on how to play
+print("'r' = Rock")
+print("'p' = Paper")
+print("'s' = Scissors")
+print()
+print("Please select a 'draw':")
+#Storing user's choice of rps
+mychoice = input()
+#generating cpu's choice
+cpuchoice = ["Rock", "Paper", "Scissors"]
+
